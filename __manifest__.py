c8cb34086b7b13367ad52dc8c43aca340eea0535
Antonio Queb <cardstillo@gmail.com>
2025-12-25 23:54:53 -0600

    Actualizar información del módulo: mejorar la descripción, autor, sitio web y licencia en el manifiesto.

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,21 @@
     'version': '1.0',
     'category': 'Inventory',
     'summary': 'Interfaz dinámica para creación de productos con lógica de ramificación',
-    'author': 'Odoo Expert UI/UX',
+    'description': """
+Generador Artístico de Productos Pro
+==========================================================================
+Módulo avanzado para la creación dinámica de productos basados en atributos artísticos como acabado y espesor.
+Características principales:
+- Interfaz de usuario intuitiva para seleccionar atributos artísticos.
+- Creación automática de productos con nombres formateados y configuraciones específicas.
+- Gestión de inventario optimizada con seguimiento por lote y unidad de medida en metros cuadrados.
+Ideal para empresas que requieren una gestión detallada de productos personalizados.
+    """,
+    'author': 'Alphaqueb Consulting',
+    'website': 'https://www.alphaqueb.com',
+    'license': 'AGPL-3',
+
+
     'depends': ['base', 'stock', 'purchase', 'product'],
     'data': [
         'security/ir.model.access.csv',
